589e8edef0d6ee41139c0cb8b70cd5d7edf5ce8a
Marty McGee <support@companyjuice.com>
2021-11-15 12:46:58 -0800

    Vue.js initial install

--- public/js/threed-garden-public.js
+++ public/js/threed-garden-public.js
@@ -19,11 +19,11 @@ import { Farmbot } from "farmbot"
 /** 
  * ThreeDGarden - jQuery JavaScript???
  */
-import jQuery from "jquery"
+//import jQuery from "jquery"
 
 var SUPER_SECRET_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ1bmtub3duIiwic3ViIjoxNTI2NywiaWF0IjoxNjM2NjU3ODUxLCJqdGkiOiIwNTBiMzIxNC02NzcxLTRlYzktODhmMS02NTVhYWVhNWQzN2QiLCJpc3MiOiIvL215LmZhcm0uYm90OjQ0MyIsImV4cCI6MTY0MTg0MTg1MSwibXF0dCI6ImNsZXZlci1vY3RvcHVzLnJtcS5jbG91ZGFtcXAuY29tIiwiYm90IjoiZGV2aWNlXzE1Mjk3Iiwidmhvc3QiOiJ4aWNvbmZ1bSIsIm1xdHRfd3MiOiJ3c3M6Ly9jbGV2ZXItb2N0b3B1cy5ybXEuY2xvdWRhbXFwLmNvbTo0NDMvd3MvbXF0dCJ9.K8jzw-l_KduohGUgS_rkmNx4700XQyZX3-JCigk-5gXf4y-aemDNemzABGpIvULVj9D02RXEiLAzPtSxRpGS_AWpwd0OSU35HNO_pCL7-KLmtmpanAOxutflPe9KN_stU1AgZC42TmMYLYeMZY1ornQOZTaIgT6dYJkqJex7HV6D1LS-1yjvEdPM2E1UTh6OWUFDndq-fdwyVNuQfYdbu0CLhTua3SaNaXCueZ7qew3-XVUx48PS2X8XFACkveZbRfb55qgabT-WpcQB4KyujdKFbAotZqJeZ1_aCxTa4WT_y_zFV7gVptgeK2FAXC1DE9cH4FJXipt0AMPksQGShA";
 
-// let bot = new Farmbot({ token: SUPER_SECRET_TOKEN });
+let bot = new Farmbot({ token: SUPER_SECRET_TOKEN });
 
 // // export default bot
 
@@ -33,9 +33,9 @@ var SUPER_SECRET_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ1bmtub
 // //     return bot.moveRelative({ x: 1, y: 2, z: 3, speed: 100 })
 // //   });
 
-// console.log("bot", bot)
+console.log("bot", bot)
 
-
+/*
 (function( $ ) {
 	'use strict'
 
@@ -62,5 +62,6 @@ var SUPER_SECRET_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ1bmtub
 	console.log("bot", bot)
 
 })( jQuery )
+*/
 
 //document.getElementById('rememberme').checked = true
