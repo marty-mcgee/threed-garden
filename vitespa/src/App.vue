ca0f08feaee56ce3aa6678d76ed3a1496d6607ee
Marty McGee <support@companyjuice.com>
2022-01-04 11:12:51 -0800

    v0.3.1 vitespa - Vite (Vitesse no PWA/SSG/SSR)

--- vitespa/src/App.vue
+++ vitespa/src/App.vue
@@ -4,15 +4,15 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-useHead({
-  title: 'ThreeD Garden',
-  // meta: [
-  //   { charset: 'utf-8' },
-  //   { equiv: 'Content-Type', content: 'text/html' },
-  //   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //   { name: 'description', content: '3D Virtual Homestead' }
-  // ]
-})
+// useHead({
+//   title: 'ThreeD Garden',
+//   // meta: [
+//   //   { charset: 'utf-8' },
+//   //   { equiv: 'Content-Type', content: 'text/html' },
+//   //   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//   //   { name: 'description', content: '3D Virtual Homestead' }
+//   // ]
+// })
 </script>
 
 <template>
