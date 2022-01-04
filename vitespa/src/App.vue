3b5ce2dedc98266b827b5b39d812c00fa3a431cf
Marty McGee <support@companyjuice.com>
2022-01-03 19:52:34 -0800

    v0.3.0 vitespa - Vite (Vitesse no PWA/SSG/SSR)

--- vitespa/src/App.vue
+++ vitespa/src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+
+console.log('HEY HEY HEY -- App.vue')
+
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitespa',
-  meta: [
-    { name: 'description', content: 'Like Vitesse but without SSG' },
-  ],
+  title: 'ThreeD Garden',
+  // meta: [
+  //   { charset: 'utf-8' },
+  //   { equiv: 'Content-Type', content: 'text/html' },
+  //   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //   { name: 'description', content: '3D Virtual Homestead' }
+  // ]
 })
 </script>
 
--- vitespa/vite.config.ts
+++ vitespa/vite.config.ts
@@ -9,7 +9,7 @@ import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Markdown from 'vite-plugin-md'
 import WindiCSS from 'vite-plugin-windicss'
-// import { VitePWA } from 'vite-plugin-pwa'
+//import { VitePWA } from 'vite-plugin-pwa'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Inspect from 'vite-plugin-inspect'
 import Prism from 'markdown-it-prism'
@@ -44,6 +44,7 @@ export default defineConfig({
         'vue-i18n',
         '@vueuse/head',
         '@vueuse/core',
+				'vitest',
       ],
       dts: 'src/auto-imports.d.ts',
     }),
@@ -70,7 +71,9 @@ export default defineConfig({
     }),
 
     // https://github.com/antfu/unplugin-icons
-    Icons(),
+    Icons({
+			autoInstall: true,
+		}),
 
     // https://github.com/antfu/vite-plugin-windicss
     WindiCSS({
@@ -83,46 +86,70 @@ export default defineConfig({
       wrapperClasses: markdownWrapperClasses,
       headEnabled: true,
       markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism)
-        md.use(LinkAttributes, {
-          pattern: /^https?:\/\//,
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
-        })
+				// https://prismjs.com/
+				// @ts-expect-error types mismatch
+				md.use(Prism)
+				// @ts-expect-error types mismatch
+				md.use(LinkAttributes, {
+					pattern: /^https?:\/\//,
+					attrs: {
+						target: '_blank',
+						rel: 'noopener',
+					},
+				})
       },
     }),
 
-    // https://github.com/antfu/vite-plugin-pwa
     // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
-    //   manifest: {
-    //     name: 'Vitesse',
-    //     short_name: 'Vitesse',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: '/pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
+		// 	registerType: 'autoUpdate',
+		// 	includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+		// 	manifest: {
+		// 		name: 'ThreeD Garden',
+		// 		short_name: 'ThreeD Garden',
+		// 		description: '3D WordPress Plugin',
+		// 		theme_color: '#076AE0',
+		// 		background_color: "#222222",
+		// 		icons: [
+		// 			{
+		// 				src: '/pwa-192x192.png',
+		// 				sizes: '192x192',
+		// 				type: 'image/png',
+		// 			},
+		// 			{
+		// 				src: '/pwa-512x512.png',
+		// 				sizes: '512x512',
+		// 				type: 'image/png',
+		// 			},
+		// 			{
+		// 				src: '/pwa-512x512.png',
+		// 				sizes: '512x512',
+		// 				type: 'image/png',
+		// 				purpose: 'any maskable',
+		// 			},
+		// 		],
+		// 		//start_url: "index.php",
+		// 		//display: "standalone"
+		// 	},
+		// 	workbox: {
+		// 		swDest: path.resolve(__dirname, '../public/dist/threedgarden-sw.js'),
+		// 		// directoryIndex: path.resolve(__dirname, '../public/index.php'),
+		// 		// directoryIndex: null,
+		// 		// navigationPreload: false,
+		// 		// exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/, /\.html$/],
+		// 		// navigateFallback: '/index.php',
+		// 		// templatedURLs: {
+		// 		// 	'/scene/mcgee-home-garden/': ['../index.php']
+		// 		// }
+		// 		// globPatterns: [
+		// 		// 	// '*/*.*',
+		// 		// 	// '*.*',
+		// 		// 	'index.html'
+		// 		// ],
+		// 	},
+		// 	// injectManifest: {
+		// 	// 	swDest: path.resolve(__dirname, '../public/dist/threedgarden-sw.js')
+		// 	// },
+		// }),
 
     // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
     VueI18n({
@@ -134,7 +161,7 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-inspect
     Inspect({
       // change this to enable inspect for debugging
-      enabled: false,
+      enabled: true,
     }),
   ],
 
@@ -143,6 +170,18 @@ export default defineConfig({
       strict: true,
     },
   },
+
+	// // https://github.com/antfu/vite-ssg
+	// ssgOptions: {
+	// 	script: 'async', // 'sync' | 'async' | 'defer' | 'async defer'
+	// 	formatting: 'none',  // 'none' | 'minify' | 'prettify'
+	// 	// mock: true, // browser window, document, etc.. default false
+	// 	// includeAllRoutes: false, // default false
+	// 	// includedRoutes(routes) {
+	// 	// 	return ['index']
+	// 	// }
+	// 	// mode: 'production',
+	// },
 
   optimizeDeps: {
     include: [
@@ -155,6 +194,15 @@ export default defineConfig({
       'vue-demi',
     ],
   },
+
+	// https://github.com/vitest-dev/vitest
+	test: {
+		include: ['test/**/*.test.ts'],
+		environment: 'jsdom',
+		deps: {
+			inline: ['@vue', '@vueuse', 'vue-demi'],
+		},
+	},
 
   // config
   // root: 'src',
