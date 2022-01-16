33dd7b8e7738d6aa7875530178357e1935f18a7b
Marty McGee <support@companyjuice.com>
2022-01-15 22:40:05 -0800

    v0.4.3 Vue3 Dapp on Vite

--- vitespa/windi.config.ts
+++ vitespa/windi.config.ts
@@ -3,13 +3,16 @@ import colors from 'windicss/colors'
 import typography from 'windicss/plugin/typography'
 
 export default defineConfig({
+
   darkMode: 'class',
+
   // https://windicss.org/posts/v30.html#attributify-mode
   attributify: true,
 
   plugins: [
     typography(),
   ],
+
   theme: {
     extend: {
       typography: {
@@ -40,4 +43,5 @@ export default defineConfig({
       },
     },
   },
+  
 })
