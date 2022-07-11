6bcf60b73c3bcfc036c0d2493f7c7301475cb2a7
Marty McGee <support@companyjuice.com>
2022-07-11 08:27:29 -0700

    v0.7.7 vitegarden main.App

---
+++ packages/vitegarden/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>HEY HEY HEY</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    
+  },
+
+  computed: {
+
+  },
+  watch: {
+
+  },
+  mounted: function () {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeUnmount: function () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      if (window.innerWidth > 1200) {
+        // this.$store.dispatch('setIsLargeScreen', true)
+      } else {
+        // this.$store.dispatch('setIsLargeScreen', false)
+      }
+      // xs: 0,
+      // sm: 576px,
+      // md: 768px,
+      // lg: 992px,
+      // xl: 1200px,
+      // xxl: 1400px
+    }
+  }
+};
+</script>
--- packages/vitegarden/src/main.js
+++ packages/vitegarden/src/main.js
@@ -1,1 +1,9 @@
-console.log("HEY HEY HEY -- ViteGarden [MM]")
+import { createApp } from "vue"
+import App from "./App.vue"
+
+console.log("ViteGarden [MM] -- HEY HEY HEY")
+
+const appInstance = createApp(App)
+appInstance.mount("#app")
+
+console.log("ViteGarden [MM] -- APP LOADED")
