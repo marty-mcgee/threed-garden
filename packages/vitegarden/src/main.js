9be11aece33bf809326cb601408c1e4a058f96da
Marty McGee <support@companyjuice.com>
2022-07-12 04:57:05 -0700

    v0.7.7 vitegarden router

--- packages/vitegarden/src/main.js
+++ packages/vitegarden/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import store from "./store"
+import router from "./router"
 
 console.log("ViteGarden [MM] -- HEY HEY HEY")
 
 const appInstance = createApp(App)
 appInstance.use(store)
+appInstance.use(router)
 appInstance.mount("#app")
 
 console.log("ViteGarden [MM] -- APP MOUNTED")
--- packages/vitegarden/src/views/Sidenav/Sidenav.vue
+++ packages/vitegarden/src/views/Sidenav/Sidenav.vue
@@ -4,8 +4,15 @@
     class="my-3 overflow-auto border-0 sidenav navbar navbar-vertical navbar-expand-xs border-radius-xl"
   >
     <div class="sidenav-header">
-      <img :src="logo" class="navbar-brand-img h-100" alt="main_logo" width="100" height="100" />
-        <span class="ms-1 font-weight-bold">{{appName}}</span>
+      <i
+        id="iconSidenav"
+        class="top-0 p-3 cursor-pointer fas fa-times text-secondary opacity-5 position-absolute end-0 d-none d-xl-none"
+        aria-hidden="true"
+      ></i>
+      <router-link class="m-0 navbar-brand" :to="{ name: '/' }">
+        <img :src="logo" class="navbar-brand-img h-100" alt="main_logo" width="100" height="100" />
+        <span class="ms-1 font-weight-bold">{{title}}</span>
+      </router-link>
     </div>
     <hr class="mt-0 horizontal dark" />
   </aside>
@@ -45,9 +52,9 @@ export default {
   },
   data() {
     return {
-      appName: "ThreeD Garden",
-      color: "success",
       logo,
+      title: "ThreeD Garden",
+      color: "success",
     };
   },
   computed: {
