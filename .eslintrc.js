8f24b53fc7ea468744258ef4fe69d9fc2bd7ccdb
Marty McGee <support@companyjuice.com>
2022-07-24 01:46:29 -0700

    v0.7.11 @threed/threed-garden workspaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+  root: true,
+  env: {
+    // node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:vue/vue3-essential"
+  ],
+  parserOptions: {
+    // parser: "babel-eslint",
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "vue"
+  ],
+  rules: {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    "quotes": ["error", "double"],
+    "semi": ["error", "never"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 }
