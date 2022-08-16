1e5f03b893f7b1fce22f94c3431f88e551dbd6e9
Marty McGee <support@companyjuice.com>
2022-08-15 19:20:51 -0700

    v0.8.0 @threed/reactgarden threedLinks

--- packages/reactgarden/src/layouts/threedLinks.tsx
+++ packages/reactgarden/src/layouts/threedLinks.tsx
@@ -1,8 +1,3 @@
-// const ThreeDLinks = {} // as Object
-// const ThreeDLinks = () => <nav>ThreeDLinks</nav> // as function component FC returning TSX
-// export default () => ()
-// export default ThreeDLinks // export module
-
 import Link from 'next/link'
 
 const ThreeDLinks = () => {
