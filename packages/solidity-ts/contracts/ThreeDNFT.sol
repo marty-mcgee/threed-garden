d3ec41e9ed1669caa588631d2034ef77801035ad
Marty McGee <support@companyjuice.com>
2022-11-11 12:09:41 -0800

    v0.12.0a2 ThreeDGarden<FC,R3F,ETH>: 🌱 Ethereum

--- packages/solidity-ts/contracts/ThreeDNFT.sol
+++ packages/solidity-ts/contracts/ThreeDNFT.sol
@@ -12,12 +12,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
-contract YourNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract ThreeDNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   //
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721("YourNFT", "YNFT") {}
+  constructor() ERC721("ThreeDNFT", "THREEDNFT") {}
 
   function _baseURI() internal view virtual override returns (string memory) {
     return "https://ipfs.io/ipfs/";
