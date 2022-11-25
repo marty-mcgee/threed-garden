4ff023757783f93120cc6557818d07a2e52e3d74
Marty McGee <support@companyjuice.com>
2022-11-25 10:36:05 -0800

    v0.12.0b3 🥕 ThreeD-App-TS: 🌱 Next.js 13 🦁

--- packages/next13-app-ts/next.config.js
+++ packages/next13-app-ts/next.config.js
@@ -1,6 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
@@ -8,6 +6,6 @@ const nextConfig = {
     // Required:
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
