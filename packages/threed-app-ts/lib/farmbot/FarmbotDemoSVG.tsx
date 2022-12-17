55029ba4b34ce31e7238fdfb00c4364be392fb3f
Marty McGee <support@companyjuice.com>
2022-12-17 10:15:18 -0800

    v0.9.6 🥕 ThreeD-App-TS: 🌱 Next.js 13 🦁 Fiber

--- packages/threed-app-ts/lib/farmbot/FarmbotDemoSVG.tsx
+++ packages/threed-app-ts/lib/farmbot/FarmbotDemoSVG.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react'
-// import DOMPurify from 'isomorphic-dompurify'
 import parse from 'html-react-parser'
 import FarmbotSVG from '#/lib/farmbot/FarmbotSVG.js'
 
 const dirtyHTML = FarmbotSVG
-// const cleanHTML = DOMPurify.sanitize(dirtyHTML, {
-//   USE_PROFILES: { html: true, svg: true, svgFilters: true },
-// })
 const parseHTML = parse(dirtyHTML)
 
 const FarmbotDemoSVG: FC = (): JSX.Element => {
