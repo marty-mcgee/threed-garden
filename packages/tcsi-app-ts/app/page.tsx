82d56f1e2af55562327e4e8487ac3ce3bb73fd15
Marty McGee <support@companyjuice.com>
2022-12-26 08:27:49 -0800

    v0.0.1a2 TCSI-App-TS: Next.js 13

--- packages/tcsi-app-ts/app/page.tsx
+++ packages/tcsi-app-ts/app/page.tsx
@@ -3,19 +3,20 @@
 
 'use client'
 
-// ** Next Imports
+// ** Next
 import type { NextPage } from 'next'
 // import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
-// ** React Imports
+// ** React
 // import type { ReactNode } from 'react'
+// import React, { FC } from 'react'
 import { useEffect } from 'react'
 
-// ** Hook Imports
+// ** Hooks
 import { useAuth } from '#/lib/auth/hooks/useAuth'
 
-// ** Demo Data Imports
+// ** Demo Data
 // import { demos } from '#/lib/data/demos'
 
 // ** Helper Components
@@ -24,8 +25,7 @@ import Spinner from '#/ui/components/spinner'
 // ** Colorful Console Messages: Utility
 import ccm from '#/lib/utils/console-colors'
 
-// ** Scaffold-ETH Imports
-// import React, { FC } from 'react'
+// ** Types
 import type { TPageProps } from '#/lib/types/TAppProps'
 
 // ==============================================================
@@ -89,6 +89,46 @@ const AppPage: NextPage<TPageProps> = (): JSX.Element => {
 
   // ** Return JSX
   return <Spinner />
+
+  // return (
+  //   <div className="space-y-8">
+  //     <h1 className="text-xl font-medium text-gray-300">
+  //       HEY HEY HEY
+  //     </h1>
+  //     <div className="space-y-10 text-white">
+  //       {demos.map((section) => {
+  //         return (
+  //           <div key={section.name} className="space-y-5">
+  //             <div className="text-xs font-semibold uppercase tracking-wider text-gray-500">
+  //               {section.name}
+  //             </div>
+  //             <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+  //               {section.items.map((item) => {
+  //                 return (
+  //                   <Link
+  //                     href={`/${item.slug}`}
+  //                     key={item.name}
+  //                     className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
+  //                   >
+  //                     <div className="font-medium text-gray-200 group-hover:text-gray-50">
+  //                       {item.name}
+  //                     </div>
+
+  //                     {item.description ? (
+  //                       <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
+  //                         {item.description}
+  //                       </div>
+  //                     ) : null}
+  //                   </Link>
+  //                 );
+  //               })}
+  //             </div>
+  //           </div>
+  //         );
+  //       })}
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default AppPage
