e4d1d6edba3af9f95adcc8beb7d6830e0f4bda03
Marty McGee <support@companyjuice.com>
2023-10-11 11:30:55 -0700

    🥕 ThreeD Garden v0.15.0a10 🌱 Next.js, TS, MUI

--- lib/threed/components/canvas/index.jsx
+++ lib/threed/components/canvas/index.jsx
@@ -87,14 +87,8 @@ export default function ThreeDCanvas({ models, children }) {
     }
   }
 
-  // create React references
-  const refCanvas = useRef()
-  const refThreeD = useRef()
-  const refCharacter = useRef()
-
   return (
     <Canvas
-      ref={refCanvas}
       camera={{ position: [-10, 10, 100], fov: 50 }}
       dpr={[1, 2]}
       shadows
@@ -213,7 +207,6 @@ export default function ThreeDCanvas({ models, children }) {
           {/* <Stacy position={[1.25, 0.7, 3.25]} scale={5.0} /> */}
           <Character
             type='gardener'
-            ref={refCharacter}
             state={state}
             threedId={2}
             threed={{}}
@@ -242,7 +235,7 @@ export default function ThreeDCanvas({ models, children }) {
 
         {/* [MM] HEY HEY HEY */}
         {/* NEED TO SEND A THREED_SCENE TO A CANVAS, BUT THIS IS FINE FOR NOW */}
-        <ThreeD ref={refThreeD} state={state} threedId={1} threed={{}} />
+        <ThreeD state={state} threedId={1} threed={{}} />
         {/* [MM] HEY HEY HEY */}
 
         {/* <Stage environment="forest" intensity={0.7}> */}
