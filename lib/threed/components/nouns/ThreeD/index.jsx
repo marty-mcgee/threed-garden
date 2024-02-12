715d605862cefcefb6ccc787c8c11ee9255fda62
Marty McGee <support@companyjuice.com>
2024-02-12 11:56:22 -0800

    🥕 ThreeD Garden v0.15.0a28 🌱 Next.js, Auth, 3D

--- lib/threed/components/nouns/ThreeD/index.jsx
+++ lib/threed/components/nouns/ThreeD/index.jsx
@@ -39,9 +39,9 @@ const defaults = {
 
   // set a default file to load for Model (for testing)
   // fileUrlDefault: '/objects/examples/compressed.glb' | '/objects/examples/compressed-v002.glb' |
-  fileUrlDefault:
-  '/objects/threeds/synty/polygon/farm/Demo/Polygon_Farm_Demo_FBX.fbx',
-  // 'https://threedpublic.s3.us-west-2.amazonaws.com/assets/threeds/synty/polygon/farm/Demo/Polygon_Farm_Demo_FBX.fbx',
+  // fileUrlDefault:
+  // '/objects/threeds/synty/polygon/farm/Demo/Polygon_Farm_Demo_FBX.fbx',
+  'https://threedpublic.s3.us-west-2.amazonaws.com/assets/threeds/synty/polygon/farm/Demo/Polygon_Farm_Demo_FBX.fbx',
   // 'https://threedpublic.s3.us-west-2.amazonaws.com/assets/threeds/synty/polygon/farm/Characters/SK_Chr_Farmer_Male_01.fbx',
   // 'https://threedpublic.s3.us-west-2.amazonaws.com/assets/threeds/synty/polygon/farm/FBX/SM_Prop_Bed_01.fbx',
 
