65b207a7ffbabfa6f475daac2a5f35da17a018fd
Marty McGee <support@companyjuice.com>
2024-04-15 04:41:55 -0700

    🥕 v0.15.1-a.0 🌱 ThreeD Garden

--- lib/threed/components/canvas/Canvas.jsx
+++ lib/threed/components/canvas/Canvas.jsx
@@ -50,21 +50,18 @@ import {
 } from '@react-three/drei'
 
 // ** JOYSTICK Imports
-// import { Canvas } from '@react-three/fiber'
 import { Physics } from '@react-three/rapier'
-// import { Environment, KeyboardControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-// import { Suspense } from 'react'
-// import Ecctrl, { EcctrlAnimation, EcctrlJoystick } from '#/lib/ecctrl/src/Ecctrl'
-import Ecctrl from '#/lib/ecctrl/src/Ecctrl'
-// import { EcctrlAnimation } from '#/lib/ecctrl/src/Ecctrl'
-import { EcctrlAnimation } from '#/lib/ecctrl/src/EcctrlAnimation'
-// import { EcctrlJoystick } from '#/lib/ecctrl/src/Ecctrl'
-import { EcctrlJoystick } from '#/lib/ecctrl/src/EcctrlJoystick'
+// // import Ecctrl, { EcctrlAnimation, EcctrlJoystick } from '#/lib/ecctrl/src/Ecctrl'
+// import Ecctrl from '#/lib/ecctrl/src/Ecctrl'
+// // import { EcctrlAnimation } from '#/lib/ecctrl/src/Ecctrl'
+// import { EcctrlAnimation } from '#/lib/ecctrl/src/EcctrlAnimation'
+// // import { EcctrlJoystick } from '#/lib/ecctrl/src/Ecctrl'
+// import { EcctrlJoystick } from '#/lib/ecctrl/src/EcctrlJoystick'
 // Components
 import Lights from './Lights'
-import Map from './Map'
-import CharacterModel from './CharacterModel'
+// import Map from './Map'
+// import CharacterModel from './CharacterModel'
 
 // ** ThreeD Imports
 // import ThreeDScenes from '#/lib/threed/components/nouns/Scene/Scene'
@@ -288,9 +285,9 @@ export function ThreeDCanvas({ _id, threeds }) { // , sceneState ??
   // **
   return (
     <>
-      <EcctrlJoystick
+      {/* <EcctrlJoystick
         buttonNumber={5}
-      />
+      /> */}
       <Canvas
         // id={_id}
         camera={{ position: [-16, 16, 16], fov: 80 }}
