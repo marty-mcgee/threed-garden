e820d12dbeaf373a09095537e10ff3f0d957a6d3
Marty McGee <support@companyjuice.com>
2024-05-03 10:38:54 -0700

    🥕 v0.15.1-b.9 🌱 ThreeD Garden: Experience: World: Character Animation + Physics + State

--- lib/threed/components/canvas/Floor.tsx
+++ lib/threed/components/canvas/Floor.tsx
@@ -9,7 +9,7 @@ export default function Floor() {
     <RigidBody type='fixed' position={[0, 0, 0]}>
       <mesh receiveShadow>
         <boxGeometry args={[2048, 0, 2048]} />
-        <meshPhongMaterial color='black' opacity={0.7} transparent={true} />
+        <meshPhongMaterial color='black' opacity={0.5} transparent={true} />
       </mesh>
     </RigidBody>
   )
