3eb578a0e2086a8553d26432a8d2dfe3a85bd7be
Marty McGee <support@companyjuice.com>
2024-05-28 19:49:36 -0700

    🥕 v0.16.0-a.9 🌱 ThreeD Garden: 3D FarmBot

--- next.config.js
+++ next.config.js
@@ -166,19 +166,20 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.(ogg|mp3|wav|mpe?g)$/i,
       exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
+      type: 'asset/resource',
+      // use: [
+      //   {
+      //     loader: require.resolve('url-loader'),
+      //     options: {
+      //       limit: config.inlineImageLimit,
+      //       fallback: require.resolve('file-loader'),
+      //       publicPath: `${config.assetPrefix}/_next/static/images/`,
+      //       outputPath: `${isServer ? '../' : ''}static/images/`,
+      //       name: '[name]-[hash].[ext]',
+      //       esModule: config.esModule || false,
+      //     },
+      //   },
+      // ],
     })
 
     // shader support
