b379b7598fb7769cfce45951005a8d9b288ac12d
Marty McGee <support@companyjuice.com>
2024-05-30 09:48:03 -0700

    🥕 v0.16.0-a.9 🌱 ThreeD Garden: 3D FarmBot

--- next.config.js
+++ next.config.js
@@ -136,7 +136,7 @@ const nextConfig = {
     // // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/avif', 'image/webp'],
     // // minimumCacheTTL: 60,
-    disableStaticImages: true,
+    disableStaticImages: true, // for avif image files to work properly
     // dangerouslyAllowSVG: true,
     // contentSecurityPolicy: 'default-src 'self'; script-src 'none'; sandbox;',
   },
