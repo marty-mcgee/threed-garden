048a6f8ca04cb5fa683562ee8034903fa212ced6
Marty McGee <support@companyjuice.com>
2024-06-05 14:33:54 -0700

    🥕 v0.16.0-b.0 🌱 ThreeD Garden: 3D FarmBot

--- src/layout/main-nav.tsx
+++ src/layout/main-nav.tsx
@@ -29,7 +29,7 @@ export function MainNav() {
           THREED GARDEN
         </CustomLink>
       </h3>
-      {/* <NavigationMenu>
+      <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Participate</NavigationMenuTrigger>
@@ -72,33 +72,33 @@ export function MainNav() {
             </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
-      </NavigationMenu> */}
+      </NavigationMenu>
     </div>
   )
 }
 
-// const ListItem = React.forwardRef<
-//   React.ElementRef<'a'>,
-//   React.ComponentPropsWithoutRef<'a'>
-// >(({ className, title, children, ...props }, ref) => {
-//   return (
-//     <li>
-//       <NavigationMenuLink asChild>
-//         <a
-//           ref={ref}
-//           className={cn(
-//             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-//             className
-//           )}
-//           {...props}
-//         >
-//           <div className='text-sm font-medium leading-none'>{title}</div>
-//           <p className='text-sm leading-snug line-clamp-2 text-muted-foreground'>
-//             {children}
-//           </p>
-//         </a>
-//       </NavigationMenuLink>
-//     </li>
-//   )
-// })
-// ListItem.displayName = 'ListItem'
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='text-sm leading-snug line-clamp-2 text-muted-foreground'>
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = 'ListItem'
