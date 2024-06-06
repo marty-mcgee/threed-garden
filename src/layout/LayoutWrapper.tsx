74405d71c157fe7de2c5a11619c5b9daeeeff1ce
Marty McGee <support@companyjuice.com>
2024-06-06 13:35:22 -0700

    🥕 v0.16.0-b.1 🌱 ThreeD Garden: 3D FarmBot

--- src/layout/LayoutWrapper.tsx
+++ src/layout/LayoutWrapper.tsx
@@ -1,36 +1,38 @@
-'use client'
+// 'use client'
 
 // SESSSSSSION PROVIDERRRRRR
 import { SessionProvider } from 'next-auth/react'
-// import { auth } from 'auth' // this goes in page
+import { auth } from '#/lib/auth/auth' // this goes in page?
 
-import { useRef } from 'react'
+// import { useRef } from 'react'
 // import dynamic from 'next/dynamic'
 // const ExperienceViewer = dynamic(() => import('#/lib/threed/components/canvas/ExperienceViewer'), { ssr: false })
 
-const LayoutWrapper = ({ children }: { children: any}) => {
+const LayoutWrapper = async ({ children }: { children: any}) => {
   const id: string = 'HEYHEYHEY'
-  const ref: any = useRef<any>()
+  // const ref: any = useRef<any>()
 
   // this goes in page?
+  let session = await auth()
+  console.debug('auth() session', session)
   // const session = async () => await auth()
   // console.debug('session', session())
 
-  let session = {
-    user: {
-      name: 'string',
-      email: 'string',
-      image: 'string',
-    },
-    expires: Date.now().toLocaleString() // 'datetime as type date or string?'
-  }
+  // let session = {
+  //   user: {
+  //     name: 'string',
+  //     email: 'string',
+  //     image: 'string',
+  //   },
+  //   expires: Date.now().toLocaleString() // 'datetime as type date or string?'
+  // }
   
   return (
     <SessionProvider session={session}>
       <div
         id='ThreeDAppSession'
         key={id}
-        ref={ref}
+        // ref={ref}
       >
         {children}
       </div>
