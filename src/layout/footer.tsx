9ef20695b1944b098497ddbee19a0dbf9050febf
Marty McGee <support@companyjuice.com>
2024-06-18 14:08:51 -0700

    🥕 v0.16.0-b.5 🌱 ThreeD Garden: Next-Themes Radix-UI

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -5,34 +5,30 @@ export default function Footer() {
   // removed className="w-full"
   return (
     <footer 
-      // className="flex flex-col px-4 mx-0 my-2 space-y-1 text-sm md:my-12 md:mx-auto sm:px-4 md:h-5 md:items-center md:space-y-0 md:space-x-2 md:flex-row"
-      style={{float: 'right', fontSize: '12px'}}
+      className="flex flex-col px-4 mx-0 my-2 space-y-1 text-sm md:my-12 md:mx-auto sm:px-4 md:h-5 md:items-center md:space-y-0 md:space-x-2 md:flex-row"
+      style={{float: 'right'}}
     >
-      &nbsp; &nbsp;
       <CustomLink href="https://github.com/marty-mcgee/threed-garden/#readme">
-        docs
+        Documentation
       </CustomLink>
-      &nbsp; &nbsp;
       <CustomLink href="https://github.com/marty-mcgee/threed-garden">
-        source
+        Source on GitHub
       </CustomLink>
-      &nbsp; &nbsp;
       <CustomLink href="https://www.npmjs.com/package/threed-garden">
-        packages
+        JS Packages
       </CustomLink>
-      &nbsp; &nbsp;
       <CustomLink href="https://threedgarden.com/participate">
-        version: {version}
+        Version: {version}
       </CustomLink>
-      &nbsp; &nbsp;
       <CustomLink href="/policy">
-        policies 
+        Policies
       </CustomLink>
-      &nbsp; &nbsp;
+      <CustomLink href="/home">
+        Home
+      </CustomLink>
       <CustomLink href="/">
-        |
+        App
       </CustomLink>
-      &nbsp; &nbsp;
     </footer>
   )
 }
