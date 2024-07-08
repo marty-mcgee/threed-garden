21bdf97b415a8dd9932187800d4c56557d89584f
Marty McGee <support@companyjuice.com>
2024-07-08 11:23:31 -0700

    🥕 v0.16.0-b.11 🌱 ThreeD Garden: Models + Characters + Controls

--- src/lib/threed/components/canvas/ExperienceViewer.tsx
+++ src/lib/threed/components/canvas/ExperienceViewer.tsx
@@ -269,14 +269,14 @@ const ThreeDExperienceViewer = (
           <ThreeDExperience threeds={threeds} />
 
           {/* SHADOW EFFECTS */}
-          <ContactShadows
+          {/* <ContactShadows
             position={[0, -1.4, 0]}
             opacity={0.75}
             scale={10}
             blur={2.5}
             far={4}
           />
-          <BakeShadows />
+          <BakeShadows /> */}
 
           {/* Transform Model using TransformControls */}
           {/*
