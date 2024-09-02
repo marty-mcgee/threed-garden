c9024b72e3aeca0ff89f2482441a1181ea1b1dfe
Marty McGee <support@companyjuice.com>
2024-09-02 13:06:45 -0700

    🥕 v0.16.0-c.5 🌱 ThreeD Garden: WordPress Plugin: CMS ThreeD + FarmBot JS + GA Tracking (no pwa)

--- next.config.js
+++ next.config.js
@@ -281,25 +281,25 @@ const nextConfig = {
 } // end nextConfig
 
 // ** WITHOUT NEXT-PWA
-// module.exports = nextConfig
+module.exports = nextConfig
 
-// ** WITH NEXT-PWA
-const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
-// ** EXPORT MODULE
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [[withPWA], [withBundleAnalyzer, {}]]
+// // ** WITH NEXT-PWA
+// const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
+// // ** EXPORT MODULE
+// module.exports = (_phase, { defaultConfig }) => {
+//   const plugins = [[withPWA], [withBundleAnalyzer, {}]]
 
-  const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
-    ...defaultConfig,
-    ...nextConfig,
-  })
+//   const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
+//     ...defaultConfig,
+//     ...nextConfig,
+//   })
 
-  const finalConfig = {}
-  Object.keys(wConfig).forEach((key) => {
-    if (!KEYS_TO_OMIT.includes(key)) {
-      finalConfig[key] = wConfig[key]
-    }
-  })
+//   const finalConfig = {}
+//   Object.keys(wConfig).forEach((key) => {
+//     if (!KEYS_TO_OMIT.includes(key)) {
+//       finalConfig[key] = wConfig[key]
+//     }
+//   })
 
-  return finalConfig
-}
+//   return finalConfig
+// }
