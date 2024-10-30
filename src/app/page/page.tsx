aebf6d92733aaae5e2fde81c2b1cce221d3b2a79
Marty McGee <support@companyjuice.com>
2024-10-30 08:09:54 -0700

    🥕 v0.17.0-a4 🌱 ThreeD: ConfigSettings

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,4 +1,8 @@
-// 'use client'
+'use client'
+
+// ** LAYOUT Components (Head, Body, Foot)
+import Header from '#/layout/header'
+import Footer from '#/layout/footer'
 
 // import type { NextPage } from 'next'
 import type { TNextPageWithProps } from '#/lib/types/TAppProps'
@@ -6,10 +10,7 @@ import { GetServerSideProps } from 'next'
 import { IPage, IBooleans, IValues, IStructures } from '#/lib/types/interfaces' // "@threed/garden/ts"
 import type { TPage } from '#/lib/types/interfaces'
 
-// import axios from 'axios'
-
 // import NextApp from 'next/app'
-
 // import { NextRequest, userAgent } from 'next/server'
 
 // export function middleware(request: NextRequest) {
@@ -33,84 +34,33 @@ import type { TPage } from '#/lib/types/interfaces'
 //   </main>
 // )
 
-// const Pages: TNextPageWithProps<{ pages: IPage[] }> = ({ pages }) => {
-const Pages: TNextPageWithProps = (pages: any): JSX.Element => {
-  const word = 'word'
+// const Page: TNextPageWithProps<{ pages: IPage[] }> = ({ pages }) => {
+const Page: TNextPageWithProps = (pages: any): JSX.Element => {
+  const word = 'ThreeD Garden'
   // const userAgent = middleware
   return (
-    <main style={{ padding: '1em' }}>
-      <h1>[MM] Boilerplate Page</h1>
-      <div>User Agent: {word} ???</div>
-      {/* {pages.map(
-        (
-          page: IPage // ooooo, interesting [MM] HEY HEY HEY
-        ) => (
-          <div
-            key={Math.floor(100000 + Math.random() * 900000)}
-            style={{ display: 'inline-flex' }}
-          >
-            <div>|</div>
-            <div key={page.title.rendered}>{page.title.rendered}</div>
-          </div>
-        )
-      )} */}
-    </main>
+    <>
+      <Header />
+      <main style={{ padding: '1em' }}>
+        <h1>[ThreeD] Boilerplate Page</h1>
+        <div>User Agent: {word}</div>
+        {/* {pages.map(
+          (
+            page: IPage // ooooo, interesting [MM] HEY HEY HEY
+          ) => (
+            <div
+              key={Math.floor(100000 + Math.random() * 900000)}
+              style={{ display: 'inline-flex' }}
+            >
+              <div>|</div>
+              <div key={page.title.rendered}>{page.title.rendered}</div>
+            </div>
+          )
+        )} */}
+      </main>
+      <Footer />
+    </>
   )
 }
 
-// NextApp.getInitialProps = async ({ req }) => {
-//   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-//   const title = { rendered: "HEY HEY HEY" }
-//   return { title, userAgent }
-// }
-Pages.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  const title = { rendered: 'HEY HEY HEY' }
-  return { title, userAgent }
-}
-// PagePage.getLayout = (page: any) => <DashboardLayout>{page}</DashboardLayout>
-// Pages.getLayout = (page: any) => <BlankLayout>{page}</BlankLayout>
-Pages.getLayout = (page: any) => {page}
-Pages.authGuard = true
-Pages.guestGuard = true
-
-// export const getServerSideProps: GetServerSideProps = async (req) => {
-//   let res = { data: [{ title: { rendered: 'HEY HEY HEY' } }] }
-//   console.log("let res", res)
-
-//   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-//   const title = { rendered: "HEY HEY HEY" }
-//   return { title, userAgent }
-
-//   try {
-//     // REST GET resPONSE
-//     res = await axios.get<IPage[]>(`${process.env.WP_REST_API_URL}/pages`)
-//     // console.log("try res", res)
-//   } catch (e: any) {
-//     res.data = [{ title: { rendered: 'NOPE NOPE NOPE' } }]
-//     // console.log("catch e", e)
-//   }
-//   // console.log("res.data", res.data)
-
-//   // const userAgent = req ? req.headers["user-agent"] : navigator.userAgent
-
-//   return {
-//     props: { pages: res.data }, // ua: userAgent
-//   }
-// }
-
-export default Pages
-
-// ===========================================================================
-// examples
-const flags: IBooleans = { read: true, write: false, delete: false }
-
-const userFile: IValues = { user: 1, username: 'One', file: 'types.txt' }
-
-const userContext: IStructures = {
-  file: userFile,
-  permissions: flags,
-  counts: { views: 3, writes: 1 }, // => INumbers (lint: try to remove IValues from IStructures)
-}
-// end examples
-// ===========================================================================
+export default Page
