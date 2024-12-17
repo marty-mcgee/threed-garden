e802adb54f4f7a46071e602576f9127d46356fe0
Marty McGee <support@companyjuice.com>
2024-12-17 07:26:22 -0800

    🥕 v0.17.0-b12 🌱 ThreeD: Home Design

--- src/layout/SessionWrapper.tsx
+++ src/layout/SessionWrapper.tsx
@@ -2,30 +2,9 @@
 // 'use server'
 // ^^ this file uses client pragma
 
-
 // ** NEXT AUTH -- SESSION PROVIDER + MIDDLEWARE CONFIG
 import { SessionProvider } from 'next-auth/react'
 // import { auth } from '#/lib/auth/auth'
-
-// ** CLERK AUTH -- External User Authentication
-import { ClerkProvider } from '@clerk/nextjs'
-// ** CLERK AUTH -- HOOKS
-// import { useAuth } from '@clerk/nextjs'
-
-// export function ClerkHookExample() {
-//   const { isLoaded, userId, sessionId, getToken } = useAuth()
-
-//   // In case the user signs out while on the page.
-//   if (!isLoaded || !userId) {
-//     return null
-//   }
-
-//   return (
-//     <div>
-//       Hello, {userId} your current active session is {sessionId}
-//     </div>
-//   )
-// }
 
 // ** EXPORT function -- async?
 export default function SessionWrapper({ children }: { children: any }) {
@@ -51,14 +30,10 @@ export default function SessionWrapper({ children }: { children: any }) {
   }
   
   return (
-    <>
-    <ClerkProvider>
-      <SessionProvider 
-        session={sessionThreeD}
-      >
-        {children}
-      </SessionProvider>
-    </ClerkProvider>
-    </>
+    <SessionProvider 
+      session={sessionThreeD}
+    >
+      {children}
+    </SessionProvider>
   )
 }
