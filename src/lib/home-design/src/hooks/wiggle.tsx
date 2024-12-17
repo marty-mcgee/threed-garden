4a6c511cc4fca64ddfc5753f0453eeeed7629eab
Marty McGee <support@companyjuice.com>
2024-12-17 07:22:40 -0800

    🥕 v0.17.0-b12 🌱 ThreeD: Home Design

--- src/lib/home-design/src/hooks/wiggle.tsx
+++ src/lib/home-design/src/hooks/wiggle.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useCallback } from "react";
-import { useSpring } from "react-spring";
-import { usePrefersReducedMotion } from "./prefers-reduced-motion";
+import React, { useState, useCallback } from "react"
+import { useSpring } from "react-spring"
+import { usePrefersReducedMotion } from "./prefers-reduced-motion"
 // Heavily inspired by Josh Comeau: https://www.joshwcomeau.com/react/boop/ 💖
 
 // Wiggle function accepts various parameters specifying properties for the animation
@@ -16,10 +16,10 @@ export function useWiggle({
   }
 }) {
   // Accessibility setting from the user system indicating that they prefer to minimize motion
-  const prefersReducedMotion = usePrefersReducedMotion();
+  const prefersReducedMotion = usePrefersReducedMotion()
 
   // Declare state variable isActive, set initially to false
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
   // We offload the actual animation to spring: https://www.react-spring.io/docs/hooks/use-spring
   const style = useSpring({
@@ -27,29 +27,29 @@ export function useWiggle({
       ? `translate(${x}px, ${y}px) rotate(${rotation}deg) scale(${scale})`
       : `translate(0px, 0px) rotate(0deg) scale(1)`,
     config: springConfig
-  });
+  })
 
   // Timing parameter determines how long the wiggle lasts using browser setTimeout function
   // React useEffect function https://reactjs.org/docs/hooks-effect.html
   React.useEffect(() => {
     if (!isActive) {
-      return;
+      return
     }
     const timeoutId = window.setTimeout(() => {
-      setIsActive(false);
-    }, timing);
+      setIsActive(false)
+    }, timing)
     return () => {
-      window.clearTimeout(timeoutId);
-    };
-  }, [isActive]); // Continue wiggle until isActive is set false when timeout elapses
+      window.clearTimeout(timeoutId)
+    }
+  }, [isActive]) // Continue wiggle until isActive is set false when timeout elapses
 
   // Set wiggle to active when the triggering event occurs - will be set false when effect completes above
   const trigger = useCallback(() => {
-    setIsActive(true);
-  }, []);
+    setIsActive(true)
+  }, [])
 
-  let appliedStyle = prefersReducedMotion ? {} : style;
+  let appliedStyle = prefersReducedMotion ? {} : style
 
   // Return animation style effect and function to apply on trigger in page
-  return [appliedStyle, trigger];
+  return [appliedStyle, trigger]
 }
