a538b767b83cbae516204ffcc64390d637dd511a
Marty McGee <support@companyjuice.com>
2024-12-17 07:28:24 -0800

    🥕 v0.17.0-b12 🌱 ThreeD: Home Design

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,31 @@
 // ** NEXT AUTH
 export { auth as middleware } from '#/lib/auth/auth'
 
-// ** CLERK AUTH
-// To learn more how to use clerkMiddleware to protect pages in your app, 
-// check out https://clerk.com/docs/references/nextjs/clerk-middleware
-import { 
-  clerkMiddleware, 
-  createRouteMatcher,
-} from '@clerk/nextjs/server'
+// // ** CLERK AUTH
+// // To learn more how to use clerkMiddleware to protect pages in your app, 
+// // check out https://clerk.com/docs/references/nextjs/clerk-middleware
+// import { 
+//   clerkMiddleware, 
+//   createRouteMatcher,
+// } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/(.*)',
-  '/contact(.*)',
-  '/sign-in(.*)', 
-  '/sign-up(.*)',
-])
+// const isPublicRoute = createRouteMatcher([
+//   '/(.*)',
+//   '/contact(.*)',
+//   '/sign-in(.*)', 
+//   '/sign-up(.*)',
+// ])
 
-export default clerkMiddleware(
-  async (auth, request) => {
-    if (!isPublicRoute(request)) {
-      await auth.protect()
-    }
-  },
-  { 
-    debug: false,
-  },
-)
+// export default clerkMiddleware(
+//   async (auth, request) => {
+//     if (!isPublicRoute(request)) {
+//       await auth.protect()
+//     }
+//   },
+//   { 
+//     debug: false,
+//   },
+// )
 
 // ** middleware config
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
@@ -37,10 +37,10 @@ export const config = {
     // Skip for images + favicon.ico
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   
-    // ** CLERK AUTH
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // // ** CLERK AUTH
+    // // Skip Next.js internals and all static files, unless found in search params
+    // '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // // Always run for API routes
+    // '/(api|trpc)(.*)',
     ],
 }
