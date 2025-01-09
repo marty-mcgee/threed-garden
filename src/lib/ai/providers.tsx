d7a7365dd02f92804880a7907d668fc54fd09cdb
Marty McGee <support@companyjuice.com>
2025-01-09 09:53:57 -0800

    🥕 v0.17.0-b20 🌱 ThreeD: Home Design OpenAI

---
+++ src/lib/ai/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ThemeProviderProps } from 'next-themes/dist/types'
+import { SidebarProvider } from '@/lib/hooks/use-sidebar'
+import { TooltipProvider } from '~/src/lib/ai/ui/tooltip'
+
+export function Providers({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      <SidebarProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SidebarProvider>
+    </NextThemesProvider>
+  )
+}
---
+++ src/lib/ai/sidebar-list.tsx
@@ -0,0 +1,43 @@
+import { clearChats, getChats } from '@/app/actions'
+import { ClearHistory } from '~/src/lib/ai/clear-history'
+import { SidebarItems } from '~/src/lib/ai/sidebar-items'
+import { ThemeToggle } from '~/src/lib/ai/theme-toggle'
+import { redirect } from 'next/navigation'
+import { cache } from 'react'
+
+interface SidebarListProps {
+  userId?: string
+  children?: React.ReactNode
+}
+
+const loadChats = cache(async (userId?: string) => {
+  return await getChats(userId)
+})
+
+export async function SidebarList({ userId }: SidebarListProps) {
+  const chats = await loadChats(userId)
+
+  if (!chats || 'error' in chats) {
+    redirect('/')
+  } else {
+    return (
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <div className="flex-1 overflow-auto">
+          {chats?.length ? (
+            <div className="space-y-2 px-2">
+              <SidebarItems chats={chats} />
+            </div>
+          ) : (
+            <div className="p-8 text-center">
+              <p className="text-sm text-muted-foreground">No chat history</p>
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between p-4">
+          <ThemeToggle />
+          <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
+        </div>
+      </div>
+    )
+  }
+}
