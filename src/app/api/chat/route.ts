7dd5e10bd415b0ead7a20b5a6678b12ae7c432d7
Marty McGee <support@companyjuice.com>
2025-03-05 07:25:18 -0800

    🥕 v0.17.1-b5 🌱 ThreeD: Demo

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,6 @@
 // app/api/chat/route.js
 import OpenAI from 'openai'
 
-// const DEEPTHINK_API_URL = 'https://api.deepseek.com/v1/deepthink'
-const DEEPTHINK_API_URL = 'https://api.deepseek.com/chat/completions'
-const DEEPTHINK_API_KEY = process.env.NEXT_PUBLIC_DEEPTHINK_API_KEY
-
 const openai = new OpenAI({
   // baseURL: 'https://api.deepseek.com',
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,24 +11,18 @@ export async function POST(req: any) {
   try {
     const body = await req.json()
     const { messages } = body
+    // console.debug(completion.choices[0].message.content)
 
     const response = await openai.chat.completions.create({
       model: 'gpt-4',
       messages,
     })
-    // async analyzeText(text: string) {
-    //   const completion = await openai.chat.completions.create({
-    //     messages: [{ role: "user", content: text }],
-    //     model: "gpt-4",
-    //   })
-  
-    //   console.debug(completion.choices[0].message.content)
-    // }
 
     return new Response(JSON.stringify({ reply: response.choices[0].message.content }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     })
+    
   } catch (error) {
     console.error(error)
     return new Response(JSON.stringify({ error: 'Failed to fetch response from OpenAI' }), {
