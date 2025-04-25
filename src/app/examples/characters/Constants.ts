6c1df0331f195df972102cc865f6687f15e7be05
Marty McGee <support@companyjuice.com>
2025-04-25 08:51:00 -0700

    🥕 v0.17.1-b11 🌱 ThreeD: Characters (GraphQL API)

--- src/app/examples/characters/Constants.ts
+++ src/app/examples/characters/Constants.ts
@@ -1,9 +1,9 @@
 export const CHARACTER_MODEL_URL = new URL(
-  `../../../../public/assets/characters/Character.glb`,
+  `/public/assets/characters/Character.glb`,
   import.meta.url,
 ).href
 
 export const SCENE_MODEL_URL = new URL(
-  `../../../../public/assets/environments/scene.glb`, 
+  `/public/assets/environments/scene.glb`, 
   import.meta.url
 ).href
